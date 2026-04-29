ee8b000c768d45837371ef106e41c4678f83e495
sloviso03 <sloviso03@gmail.com>
2026-04-29 02:37:40 -0300

    nvim config

---
+++ modules/home/nvim/lua/plugins/ui.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme dracula")
+    end,
+  },
+
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      window = {
+        position = "right",
+        width = 30,
+      },
+      filesystem = {
+        follow_current_file = { enabled = true },
+      },
+    },
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    },
+  },
+
+ 
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        theme = "dracula",
+        icons_enabled = true,
+      },
+    },
+  },
+}
