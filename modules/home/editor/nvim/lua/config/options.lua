06193af515ad84b8ef35bcaf9b4412ab92e5a66c
sloviso03 <sloviso03@gmail.com>
2026-04-29 23:30:34 -0300

    nvim keybindings

--- modules/home/editor/nvim/lua/config/options.lua
+++ modules/home/editor/nvim/lua/config/options.lua
@@ -27,7 +27,6 @@ map({"n", "i"}, "<C-q>", "<cmd>q<cr>", { desc = "Quit" })
 map({"n", "i"}, "<C-z>", "<cmd>undo<cr>", { desc = "Undo" })
 map({"n", "i"}, "<C-y>", "<cmd>redo<cr>", { desc = "Redo" })
 map({"n", "i"}, "<C-f>", "/", { desc = "Search" })
-map({"n", "i"}, "<C-a>", "ggVG", { desc = "Select all" })
 map("i", "<C-v>", "<C-r>+", { desc = "Paste" })
 
 -- arrow keys work in normal mode too 
@@ -35,22 +34,6 @@ map("n", "<Up>", "k", { desc = "Up" })
 map("n", "<Down>", "j", { desc = "Down" })
 map("n", "<Left>", "h", { desc = "Left" })
 map("n", "<Right>", "l", { desc = "Right" })
-
--- shift+arrows to select
-map("i", "<S-Left>", "<C-o>vh", { desc = "Select left" })
-map("i", "<S-Right>", "<C-o>vl", { desc = "Select right" })
-map("i", "<S-Up>", "<C-o>vk", { desc = "Select up" })
-map("i", "<S-Down>", "<C-o>vj", { desc = "Select down" })
-map("n", "<S-Left>", "vh", { desc = "Select left" })
-map("n", "<S-Right>", "vl", { desc = "Select right" })
-map("n", "<S-Up>", "vk", { desc = "Select up" })
-map("n", "<S-Down>", "vj", { desc = "Select down" })
-
--- ctrl+k to cut selection
-map({"n", "v"}, "<C-k>", "d", { desc = "Cut" })
-map("i", "<C-k>", "<C-o>d", { desc = "Cut line" })
-
-
 
 -- force neo-tree to always open on the right
 vim.api.nvim_create_autocmd("VimEnter", {
@@ -60,7 +43,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
     vim.keymap.set("n", "<leader>E", "<cmd>Neotree toggle right<cr>", { desc = "Explorer (Right)" })
   end,
 })
-
 
 -- compile and run current file based on filetype
 vim.api.nvim_create_autocmd("FileType", {
