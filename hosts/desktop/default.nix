129ad7170bda38b2c78980d4390b44d9589450aa
sloviso03 <sloviso03@gmail.com>
2026-05-05 12:43:42 -0300

    video config

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -60,7 +60,17 @@
   programs.fish.enable = true;
 
   # nix settings
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSeBw="
+    ];
+  };
   nixpkgs.config.allowUnfree = true;
   virtualisation.libvirtd.enable = true;
   system.stateVersion = "24.11";
