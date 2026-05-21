b69c814eeaddf8f8727ee0a7671fd91cd23bc53d
sloviso03 <sloviso03@gmail.com>
2026-05-21 09:35:00 -0300

    editor: neovim options and performance tweaks

--- modules/home/editor/nvim/lua/config/options.lua
+++ modules/home/editor/nvim/lua/config/options.lua
@@ -21,41 +21,15 @@ opt.updatetime = 200
 
 local map = vim.keymap.set 
 -- NVIM KEYMAPS ---------------- {
- 
-  -- nano-style keymaps
   map({"n", "i"}, "<C-s>", "<cmd>w<cr>", { desc = "Save" })
-  map({"n", "i"}, "<C-q>", "<cmd>q<cr>", { desc = "Quit" })
   map({"n", "i"}, "<C-z>", "<cmd>undo<cr>", { desc = "Undo" })
-  map({"n", "i"}, "<C-y>", "<cmd>redo<cr>", { desc = "Redo" })
-  map({"n", "i"}, "<C-f>", "/", { desc = "Search" })
-  map("i", "<C-v>", "<C-r>+", { desc = "Paste" })
-
+  map({"n", "i"}, "<C-r>", "<cmd>redo<cr>", { desc = "Redo" })
 
   -- arrow keys work in normal mode too 
   map("n", "<Up>", "k", { desc = "Up" })
   map("n", "<Down>", "j", { desc = "Down" })
   map("n", "<Left>", "h", { desc = "Left" })
   map("n", "<Right>", "l", { desc = "Right" })
-
-
-  -- nano/GUI-style shift+arrow selection
-  opt.keymodel = "startsel,stopsel"   
-  opt.selectmode = "key"      
-  opt.selection = "inclusive"
-  -- shift + arrows in insert and normal mode
-  map({"i", "n"}, "<S-Left>", "<S-Left>", { desc = "Select left", noremap = true })
-  map({"i", "n"}, "<S-Right>", "<S-Right>", { desc = "Select right", noremap = true })
-  map({"i", "n"}, "<S-Up>", "<S-Up>", { desc = "Select up", noremap = true })
-  map({"i", "n"}, "<S-Down>", "<S-Down>", { desc = "Select down", desc = "Select down", noremap = true })
-
-
-  -- move lines up/down with Shift+Alt+arrows (insert and normal mode)
-  map("n", "<S-A-Up>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
-  map("n", "<S-A-Down>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
-  map("i", "<S-A-Up>", "<Esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
-  map("i", "<S-A-Down>", "<Esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
-
--- }
 
 
 -- force neo-tree to always open on the right
@@ -67,15 +41,3 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
--- compile and run current file based on filetype
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cpp", "c" },
-  callback = function()
-    map("n", "<F5>", function()
-      local file = vim.fn.expand("%:p")
-      local out  = vim.fn.expand("%:p:r")
-      vim.cmd("w")
-      vim.cmd("split | terminal g++ " .. file .. " -o " .. out .. " && " .. out)
-    end, { desc = "Compile and run", buffer = true })
-  end,
-})
